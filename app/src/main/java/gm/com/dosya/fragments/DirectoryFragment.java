f15a2713585354f93635158fad80ac431d5f94a5
mustafakuloglu <mustafakuloglu@outlook.com>
2016-07-13 15:14:10 +0300

    kopyala yapıştır(düzenlenecek)

--- app/src/main/java/gm/com/dosya/fragments/DirectoryFragment.java
+++ app/src/main/java/gm/com/dosya/fragments/DirectoryFragment.java
@@ -30,7 +30,11 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -57,7 +61,12 @@ public class DirectoryFragment extends Fragment {
     private ArrayList<HistoryEntry> history = new ArrayList<HistoryEntry>();
     private HashMap<String, ListItem> selectedFiles = new HashMap<String, ListItem>();
     private long sizeLimit = 1024 * 1024 * 1024;
-    public String silinecek = null;
+    public String ilkelPath = null;
+    String copyPath=null;
+    String ana=null;
+    String targetPath=null;
+
+    boolean cpy=false;
 
     private String[] chhosefileType = {".pdf", ".doc", ".docx", ".DOC", ".DOCX"};
     File[] files = null;
@@ -179,7 +188,9 @@ public class DirectoryFragment extends Fragment {
 
                           @Override
            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                   silinecek=items.get(position).getThumb();
+                   ilkelPath =items.get(position).getThumb();
+                              ana=items.get(position).getTitle();
+
 
                            return false;
                    }
@@ -602,8 +613,12 @@ public void onCreateContextMenu(ContextMenu menu, View v,
         super.onCreateContextMenu(menu, v, menuInfo);
         menu.setHeaderTitle("Context Menu");
         menu.add(0, v.getId(), 0, "Kopyala");
-        menu.add(0, v.getId(), 0, "Yapıştır");
         menu.add(0, v.getId(), 0, "Sil");
+        if(cpy==true)
+        {
+            menu.add(0, v.getId(), 0, "Yapıştır");
+        }
+
    }
         @Override
         public boolean onContextItemSelected(final MenuItem itemr) {
@@ -615,12 +630,78 @@ public void onCreateContextMenu(ContextMenu menu, View v,
                           .getMenuInfo();
             position = (int) info.id;
 
-                File sil = new File(silinecek);
+                File sil = new File(ilkelPath);
                 boolean deleted= sil.delete();
                 items.remove(position);
             baseAdapter.notifyDataSetChanged();
      }
+            if (itemr.getTitle() == "Kopyala")
+            {
+                copyPath=ilkelPath;
+                cpy=true;
+            }
+            if (itemr.getTitle() == "Yapıştır")
+            {File control=new File(ilkelPath);
+                if(control.isDirectory())
+                {
+                    targetPath =copyPath;
+                }
+                else {
+
+                    targetPath=ilkelPath.substring(0,ilkelPath.length()-ana.length());
+                }
+                copyFileOrDirectory(copyPath,targetPath);
+                cpy=false;
+            }
               return super.onContextItemSelected(itemr);
           }
 
+
+    public static void copyFileOrDirectory(String srcDir, String dstDir) {
+
+        try {
+            File src = new File(srcDir);
+            File dst = new File(dstDir, src.getName());
+
+            if (src.isDirectory()) {
+
+                String files[] = src.list();
+                int filesLength = files.length;
+                for (int i = 0; i < filesLength; i++) {
+                    String src1 = (new File(src, files[i]).getPath());
+                    String dst1 = dst.getPath();
+                    copyFileOrDirectory(src1, dst1);
+
+                }
+            } else {
+                copyFile(src, dst);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.getParentFile().exists())
+            destFile.getParentFile().mkdirs();
+
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
+    }
 }
