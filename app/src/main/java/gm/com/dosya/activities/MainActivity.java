5ceafdd0e20cbbf48d79d3c990b0e6b8e6ecb160
mustafakuloglu <mustafakuloglu@outlook.com>
2016-07-15 10:51:47 +0300

    Navigation drawer eklendi

--- app/src/main/java/gm/com/dosya/activities/MainActivity.java
+++ app/src/main/java/gm/com/dosya/activities/MainActivity.java
@@ -43,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
                                                      Toast.makeText(getApplicationContext(), "basıldı", Toast.LENGTH_SHORT).show();
                                                  }
                                              }
+
         );
 
 
--- app/src/main/java/gm/com/dosya/fragments/DirectoryFragment.java
+++ app/src/main/java/gm/com/dosya/fragments/DirectoryFragment.java
@@ -30,6 +30,13 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -68,6 +75,7 @@ public class DirectoryFragment extends Fragment {
     String copyPath=null;
     String itemname =null;
     String targetPath=null;
+    Toolbar toolbar;
 
 
     boolean cpy=false;
@@ -155,9 +163,6 @@ public class DirectoryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
         if (!receiverRegistered) {
             receiverRegistered = true;
             IntentFilter filter = new IntentFilter();
@@ -266,8 +271,9 @@ public class DirectoryFragment extends Fragment {
                 parent.removeView(fragmentView);
             }
         }
-       Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.tool_bar);
-
+      toolbar = (Toolbar) getActivity().findViewById(R.id.tool_bar);
+        new DrawerBuilder().withActivity(getActivity()).build();
+        drawerProcesses();
         return fragmentView;
     }
 
@@ -738,5 +744,28 @@ public class DirectoryFragment extends Fragment {
                 destination.close();
             }
         }
+    }
+    private void drawerProcesses()
+    {
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withIdentifier(1).withName("item 1");
+        SecondaryDrawerItem item2 = (SecondaryDrawerItem) new SecondaryDrawerItem().withIdentifier(2).withName(R.string.app_name);
+        Drawer result = new DrawerBuilder()
+                .withActivity(getActivity())
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName("string")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        return false;
+                    }
+
+                    })
+                .build();
+
     }
 }
